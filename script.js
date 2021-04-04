3bd54ef6fcdac5b3f524f93e9b3b1adb19325af2
mithlesharya <info.mithlesharya@gmail.com>
2021-04-04 22:29:27 +0530

    This is a glass morphosim credit card design

---
+++ script.js
@@ -0,0 +1,16 @@
+/*******************************************
+Hey!
+I hope you loved this little handicraft of
+mine.
+If yes, then it won't be a bad idea to color
+that Grey Heart, Red. Look up! What do you think about
+it? It costs you $0.00 only. So...
+
+Author:
+Shounak Das @ https://dev.to/dasshounak
+
+Design adapted from:
+Sarah Newman @ https://www.behance.net/nwmn
+********************************************/
+
+alert("Please leave a ❤ if you are not upset with it.");
